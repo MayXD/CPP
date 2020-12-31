af8b1476d97743a9c2392aa5333023493d8dcca1
MayXD <1096may1559629925@gmail.com>
2020-12-31 16:57:07 +0800

    test

---
+++ git_init.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+
+#git init
+
+git config --global user.name "MayXD" 
+git config --global user.email "1096may1559629925@gmail.com"
+
+#git pull origin master
+#git add -A
+#git commit -m 'auto commit'
+#git push origin master
+
